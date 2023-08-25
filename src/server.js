ac8d16e9e91e6fdf4ae4c2878e52792c145ad035
jonmontaluisa <jonmontaluisa@gmail.com>
2023-08-25 14:22:48 -0500

    logging config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import app from './app';
 import config from './config';
 
+if (config.environment === 'development') { console.log(config) };
+
 const port = config.port;
 
 app.listen(port, () => {
